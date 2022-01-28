edcec456ea171bc2e233b9fbf44e9fc87cd4187a
AkolenDvdnk <ak0lenfs@gmail.com>
2022-01-28 14:00:12 +0200

    Player Positioning

---
+++ Unit 2 - Basic Gameplay/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    private float horizontalInput;
+
+    private float boundValue = 10f;
+
+    private void Update()
+    {
+        LevelBounds(); 
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
+    }
+    private void LevelBounds()
+    {
+        if (transform.position.x < -boundValue)
+        {
+            transform.position = new Vector3(-boundValue, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > boundValue)
+        {
+            transform.position = new Vector3(boundValue, transform.position.y, transform.position.z);
+        }
+    }
+}
