35614a0941459f87927193bfe5139568e78c1e62
AkolenDvdnk <ak0lenfs@gmail.com>
2022-01-29 20:22:47 +0200

    Aggressive animals

--- Unit 2 - Basic Gameplay/Assets/EnemySpawner.cs
+++ Unit 2 - Basic Gameplay/Assets/EnemySpawner.cs
@@ -12,21 +12,32 @@ public class EnemySpawner : MonoBehaviour
 
     private void Start()
     {
-        StartCoroutine(SpawnEnemy());
+        StartCoroutine(SpawnEnemyTop()); 
     }
-    private IEnumerator SpawnEnemy()
+    private IEnumerator SpawnEnemyTop()
     {
         for (int i = 0; ; i++)
         {
-            Vector3 spawnPoint = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, transform.position.z);
-            GameObject enemy = Enemy(spawnPoint);
-
+            Vector3 spawnPointTop = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, transform.position.z);
+            Vector3 rotationTop = new Vector3(0, 180, 0);
+            Enemy(spawnPointTop, Quaternion.Euler(rotationTop));
             yield return new WaitForSeconds(spawnDelay);
+
+            Vector3 spawnPointRight = new Vector3(25, 0, Random.Range(0, 18));
+            Vector3 rotationRight = new Vector3(0, -90, 0);
+            Enemy(spawnPointRight, Quaternion.Euler(rotationRight));
+            yield return new WaitForSeconds(spawnDelay);
+
+            Vector3 spawnPointLeft = new Vector3(-25, 0, Random.Range(0, 18));
+            Vector3 rotationLeft = new Vector3(0, 90, 0);
+            Enemy(spawnPointLeft, Quaternion.Euler(rotationLeft));
+            yield return new WaitForSeconds(spawnDelay);
+
         }
     }
-    private GameObject Enemy(Vector3 point)
+    private GameObject Enemy(Vector3 point, Quaternion quaternion)
     {
         int enemyIndex = Random.Range(0, enemies.Length);
-        return Instantiate(enemies[enemyIndex], point, enemies[enemyIndex].transform.rotation);
+        return Instantiate(enemies[enemyIndex], point, quaternion);
     }
 }
